4dfdb2293ccbf316d24a37e9a4582f259d74b0f1
Boris Shminke <boris@shminke.ml>
2022-06-13 10:00:26 +0200

    no full stops in Sphinx headers

--- tptp_lark_parser/cnf_parser.py
+++ tptp_lark_parser/cnf_parser.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# noqa D205
+# noqa D205, D400
 """
-CNF Parser.
+CNF Parser
 ===========
 """
 import dataclasses
--- tptp_lark_parser/tptp_parser.py
+++ tptp_lark_parser/tptp_parser.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# noqa D205
+# noqa D205, D400
 """
-TPTP Parser.
+TPTP Parser
 ============
 """
 import os
